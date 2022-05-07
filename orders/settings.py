0759426e486465deff8f3d1ad4db69afe4664b83
Igor Sverchkov <knjaz1989@gmail.com>
2022-05-07 18:56:49 +0300

    Throttle added

--- orders/settings.py
+++ orders/settings.py
@@ -141,10 +141,17 @@ SERVER_EMAIL = 'django@my-domain.com'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+            'rest_framework.throttling.AnonRateThrottle',
+            'rest_framework.throttling.UserRateThrottle'
+        ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/day',
+        'user': '100/day'
+    }
 }
